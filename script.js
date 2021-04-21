bddf714075f53e102ad37a9ca25dad3e1c945f08
chuongpnd <chuongpnd@192.168.11.20>
2021-04-21 10:40:53 +0700

    complete practices

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+  const year = document.getElementById('numberYears').value;
+  const slideRate = document.getElementById('slideRate').value;
+  const amount = document.getElementById('numberAmount').value;
+  console.log(slideRate);
+  if (amount === '') {
+    alert('Please fill amount');
+    return;
+  }
+  if (slideRate === null || slideRate === 0) {
+    alert('Please fill Rate');
+    return;
+  }
+  if (year === '') {
+    alert('Please fill Year');
+    return;
+  }
+  const total = ((amount * slideRate) / 100) * year;
+  document.getElementById('total').value = total;
 }
-        
+
+function updateTextInput(val) {
+  document.getElementById('valueRate').value = val + '%';
+}
